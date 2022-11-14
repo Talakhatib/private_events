9beb97e3513792494b328b9ef63fa130250dbab5
unknown <talakhatib4@gmail.com>
2022-11-14 16:48:38 +0200

    updating

--- app/models/attend_event.rb
+++ app/models/attend_event.rb
@@ -1,6 +1,6 @@
 class AttendEvent < ApplicationRecord
     belongs_to :attendee, class_name:"User" 
-    belongs_to :attended_event, class_name:"Event",dependent: :destroy
+    belongs_to :attended_event, class_name:"Event"
     
     scope :get_id,-> (attendee) {where('attendee_id=? ',attendee).ids}
 end
