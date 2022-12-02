170746fc7294e420516cf48fc56049bdda487b94
unknown <talakhatib4@gmail.com>
2022-12-02 18:43:03 +0200

    updating users_spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,18 +5,24 @@ RSpec.describe "Users", type: :system do
     driven_by(:rack_test)
   end
 
-  it "loads the show page with the user name" do
+  it "visiting the index page with the user name" do
     user = User.create(name: "Test",email:"test@gmail.com",password:"password",password_confirmation:"password",phone:"81234567",date_of_birth:"12/12/2012",address:"yhgygugugu")
     visit users_path
     expect(page).to have_content("Test")
   end
 
-  it "loads the show page with the user name" do
+  it "visiting show page with the user name and testing add category button" do
     user = User.create(name: "Test",email:"test@gmail.com",password:"password",password_confirmation:"password",phone:"81234567",date_of_birth:"12/12/2012",address:"yhgygugugu")
     visit user_path(user)
-    # click_on "Categories List"
     fill_in "category_title",	with: "sport festivel" 
     click_on "Add Category"
   end
+
+  it "visiting the show page with the user name and testing the link categories list" do
+    user = User.create(name: "Test",email:"test@gmail.com",password:"password",password_confirmation:"password",phone:"81234567",date_of_birth:"12/12/2012",address:"yhgygugugu")
+    visit user_path(user)
+    click_on "Categories List"
+  end
+
 
 end
